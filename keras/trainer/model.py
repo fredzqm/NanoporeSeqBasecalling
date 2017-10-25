fcb7586b5ab7f8d00c7076a31b2235f35c19cd75
Fred Zhang <fredzqm@gmail.com>
2017-10-25 14:43:56 -0400

    extract preProcesssInputModule

--- keras/trainer/model.py
+++ keras/trainer/model.py
@@ -15,10 +15,9 @@
 
 """Implements the Keras Sequential model."""
 
-import itertools
+from processInput import generator_input
 
 import keras
-import pandas as pd
 from keras import backend as K
 from keras import layers, models
 from keras.utils import np_utils
@@ -32,31 +31,6 @@ from tensorflow.python.saved_model import utils
 from tensorflow.python.saved_model import tag_constants, signature_constants
 from tensorflow.python.saved_model.signature_def_utils_impl import build_signature_def, predict_signature_def
 from tensorflow.contrib.session_bundle import exporter
-
-# csv columns in the input file
-CSV_COLUMNS = ('age', 'workclass', 'fnlwgt', 'education', 'education_num',
-               'marital_status', 'occupation', 'relationship', 'race',
-               'gender', 'capital_gain', 'capital_loss', 'hours_per_week',
-               'native_country', 'income_bracket')
-
-CSV_COLUMN_DEFAULTS = [[0], [''], [0], [''], [0], [''], [''], [''], [''],
-                       [''], [0], [0], [0], [''], ['']]
-
-# Categorical columns with vocab size
-# native_country and fnlwgt are ignored
-CATEGORICAL_COLS = (('education', 16), ('marital_status', 7),
-                    ('relationship', 6), ('workclass', 9), ('occupation', 15),
-                    ('gender', [' Male', ' Female']), ('race', 5))
-
-CONTINUOUS_COLS = ('age', 'education_num', 'capital_gain', 'capital_loss',
-                   'hours_per_week')
-
-LABELS = [' <=50K', ' >50K']
-LABEL_COLUMN = 'income_bracket'
-
-UNUSED_COLUMNS = set(CSV_COLUMNS) - set(
-    zip(*CATEGORICAL_COLS)[0] + CONTINUOUS_COLS + (LABEL_COLUMN,))
-
 
 def model_fn(input_dim,
              labels_dim,
@@ -99,50 +73,3 @@ def to_savedmodel(model, export_path):
     )
     builder.save()
 
-
-def to_numeric_features(features):
-  """Convert the pandas input features to numeric values.
-     Args:
-        features: Input features in the data
-          age (continuous)
-          workclass (categorical)
-          fnlwgt (continuous)
-          education (categorical)
-          education_num (continuous)
-          marital_status (categorical)
-          occupation (categorical)
-          relationship (categorical)
-          race (categorical)
-          gender (categorical)
-          capital_gain (continuous)
-          capital_loss (continuous)
-          hours_per_week (continuous)
-          native_country (categorical)
-  """
-
-  for col in CATEGORICAL_COLS:
-    features = pd.concat([features, pd.get_dummies(features[col[0]], drop_first = True)], axis = 1)
-    features.drop(col[0], axis = 1, inplace = True)
-
-  # Remove the unused columns from the dataframe
-  for col in UNUSED_COLUMNS:
-    features.pop(col)
-
-  return features
-
-def generator_input(input_file, chunk_size):
-  """Generator function to produce features and labels
-     needed by keras fit_generator.
-  """
-  input_reader = pd.read_csv(tf.gfile.Open(input_file[0]),
-                           names=CSV_COLUMNS,
-                           chunksize=chunk_size,
-                           na_values=" ?")
-
-  for input_data in input_reader:
-    input_data = input_data.dropna()
-    label = pd.get_dummies(input_data.pop(LABEL_COLUMN))
-
-    input_data = to_numeric_features(input_data)
-    n_rows = input_data.shape[0]
-    return ( (input_data.iloc[[index % n_rows]], label.iloc[[index % n_rows]]) for index in itertools.count() )
