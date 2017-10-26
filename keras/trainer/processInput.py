a84b718195a8628fe35a45739bd1a7b5353f3074
Fred Zhang <fredzqm@gmail.com>
2017-10-26 09:17:43 -0400

    process .signal too

--- keras/trainer/processInput.py
+++ keras/trainer/processInput.py
@@ -4,19 +4,32 @@ import json
 import sys
 import numpy as np
 
+def read_by_tokens(fileobj):
+  for line in fileobj:
+    for token in line.split():
+      yield token
+
 def generator_input(input_file, chunk_size):
+  with open(input_file[1]) as f:
+    signals = [int(token) for token in read_by_tokens(f)]
+
   dataframe = pd.read_csv(open(input_file[0], 'r'), names=['prevSig', 'sig', 'gene'], delim_whitespace=True)
   genes = pd.get_dummies(dataframe['gene'])
   dataframe.drop('gene', axis = 1, inplace = True)
   dataframe.drop('prevSig', axis = 1, inplace = True)
+  print(len(signals))
+  print(dataframe.shape)
+  print(dataframe['sig'][dataframe.shape[0]-1])
   NUM_INPUT = 10
   while True:
     for i in range(NUM_INPUT, dataframe.shape[0]):
-      yield (np.array(dataframe.iloc[i-NUM_INPUT:i+1,:]).transpose(), genes.iloc[[i]])
+      index = dataframe['sig'][i]
+      yield (np.array([signals[index-NUM_INPUT:index+1]]), genes.iloc[[i]])
 
 
 if __name__ == '__main__':
-  gen = generator_input(['keras/data/probertylist.label', 'keras/data/signalFile.signal'], chunk_size=5000)
-  sample = gen.next()
+  gen = generator_input(['keras/data/propertyList.label', 'keras/data/signalFile.signal'], chunk_size=5000)
+  sample = next(gen)
   print(type(sample))
-  print(sample)
+  print(sample[0].shape)
+  print(sample[1].shape)
--- uploadAndRun.sh
+++ uploadAndRun.sh
@@ -1,7 +1,7 @@
-rm -rf keras\output
-gcloud auth activate-service-account --key-file NanoporeSequence-e62a27da5425.json
-gcloud compute --project "nanoporesequence" ssh --zone "us-east1-b" "fredzqm@deep-learning-compute" --command "rm -rf keras/*"
-gcloud compute --project "nanoporesequence" scp --zone "us-east1-b" keras/*  fredzqm@deep-learning-compute:keras/ --recurse
-gcloud compute --project "nanoporesequence" ssh --zone "us-east1-b" "fredzqm@deep-learning-compute" --command "cd keras; gcloud ml-engine local train --module-name trainer.task --package-path trainer -- --train-files data/propertyList.label --eval-files data/propertyList.label --train-steps 100 --job-dir output --eval-steps 100"
-mkdir keras\output
-gcloud compute --project "nanoporesequence" scp --zone "us-east1-b" fredzqm@deep-learning-compute:keras/output/* keras/output --recurse
+	rm -rf keras\output
+	gcloud auth activate-service-account --key-file NanoporeSequence-e62a27da5425.json
+	gcloud compute --project "nanoporesequence" ssh --zone "us-east1-b" "fredzqm@deep-learning-compute" --command "rm -rf keras/*"
+	gcloud compute --project "nanoporesequence" scp --zone "us-east1-b" keras/*  fredzqm@deep-learning-compute:keras/ --recurse
+	gcloud compute --project "nanoporesequence" ssh --zone "us-east1-b" "fredzqm@deep-learning-compute" --command "cd keras; gcloud ml-engine local train --module-name trainer.task --package-path trainer -- --train-files data/propertyList.label data/signalFile.signal --eval-files data/propertyList.label data/signalFile.signal --train-steps 100 --job-dir output --eval-steps 100"
+	mkdir keras\output
+	gcloud compute --project "nanoporesequence" scp --zone "us-east1-b" fredzqm@deep-learning-compute:keras/output/* keras/output --recurse
