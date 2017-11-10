ac24e6b2b80882b399c35eadcdd46fb4e2dc5784
Fred Zhang <fredzqm@gmail.com>
2017-11-10 12:21:34 -0500

    OptimizeInput

--- keras/trainer/processInput.py
+++ keras/trainer/processInput.py
@@ -20,6 +20,7 @@ def downloadFile(file):
     print("downloaded: " + file)
 
 wing = 60
+excludeEdge = 1
 INPUT_SIZE = wing*2
 OUTPUT_SIZE = 4
 
@@ -36,33 +37,38 @@ except Exception:
   
 def generator_input(input_file, chunk_size):
   while True:
-    for dataSet in range(0, len(input_file), 2):
-      downloadFile(input_file[dataSet])
-      downloadFile(input_file[dataSet+1])
-      with open(input_file[dataSet+1]) as f:
-        signals = [int(token) for token in read_by_tokens(f)]
-      dataframe = pd.read_csv(open(input_file[dataSet], 'r'), names=['prevSig', 'sig', 'gene'], delim_whitespace=True)
-      # preprocess input
-      expected = [None] * len(signals)
-      itr = dataframe.iterrows()
-      try:
-        _, row = next(itr)
-        end = start = row['prevSig']
-        while end < len(expected):
-          if end >= row['sig']:
-            _, row = next(itr)
-          if end >= row['prevSig']:
-            expected[end] = row['gene']
-          end += 1
-      except StopIteration:
-        pass
-      expected = pd.get_dummies(expected)
-      for i in range(max(start, wing), min(end, len(expected)-wing-chunk_size), chunk_size):
-        takeRange = range(i, i+chunk_size)
-        inputSignals = [signals[j-wing:j+wing] for j in takeRange]
-        ouputSignals = expected.iloc[takeRange]
+    try:
+      for dataSet in range(0, len(input_file), 2):
+        downloadFile(input_file[dataSet])
+        downloadFile(input_file[dataSet+1])
+        with open(input_file[dataSet+1]) as f:
+          signals = [int(token) for token in read_by_tokens(f)]
+        dataframe = pd.read_csv(open(input_file[dataSet], 'r'), names=['prevSig', 'sig', 'gene'], delim_whitespace=True)
+        # preprocess input
+        expected = [None] * len(signals)
+        itr = dataframe.iterrows()
+        try:
+          _, row = next(itr)
+          end = start = row['prevSig']
+          while end < len(expected):
+            if end >= row['sig']:
+              _, row = next(itr)
+            if end >= row['prevSig']:
+              expected[end] = row['gene']
+            end += 1
+        except StopIteration:
+          pass
+        def filterRange(i, j):
+          for x in range(i, j):
+            if expected[x-excludeEdge] == expected[x] and expected[x+excludeEdge] == expected[x]:
+              yield x
+        expectedDummy = pd.get_dummies(expected)
+        for i in range(max(start, wing), min(end, len(expected)-wing-chunk_size), chunk_size):
+          inputSignals = [signals[j-wing:j+wing] for j in filterRange(i, i+chunk_size)]
+          ouputSignals = expectedDummy.iloc[[j for j in filterRange(i, i+chunk_size)]]
         yield (np.expand_dims(np.array(inputSignals), axis=2), ouputSignals)
-
+    except Exception as e:
+      print e
 
 if __name__ == '__main__':
   gen = generator_input(['keras/data/propertyList.label', 'keras/data/signalFile.signal'], chunk_size=50)
